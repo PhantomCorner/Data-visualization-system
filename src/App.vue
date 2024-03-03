f61495895ca545cf95b44c3f05ed1b8c59f34d7e
Jiao <jiaojiaobigbang@gmail.com>
2024-03-03 14:11:38 +1300

    login page update

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
-<Loginpage/>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script>
-import Loginpage from './components/loginPage.vue'
 export default {
-  name: 'App',
-  components: {
-    Loginpage
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: 'COMPX576 Project',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
