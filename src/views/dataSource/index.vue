7b03aca56aeeb0cd2f92db9993fc5a4a8df7b452
Jiao <jiaojiaobigbang@gmail.com>
2024-04-14 12:06:03 +1200

    fix data source upload function

--- src/views/dataSource/index.vue
+++ src/views/dataSource/index.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="dataSource-container">
+    <div style="display: flex; flex-direction: row-reverse">
+      <el-upload
+        class="upload-demo"
+        action=""
+        :show-file-list="false"
+        :http-request="handleFileChange"
+        accept=".XLSX, .XLSM, .XLS"
+      >
+        <el-button type="success">Upload</el-button>
+      </el-upload>
+      <el-button type="primary" @click="getBucketList">Refresh</el-button>
+    </div>
+
     <el-table :data="tableData" style="width: 100%">
       <el-table-column prop="Key" label="File Name">
         <template slot-scope="scope">
@@ -18,22 +31,11 @@
         </template>
       </el-table-column>
       <el-table-column prop="LastModified" label="Last Modified" />
-      <el-table-column>
-        <template slot="header" slot-scope="scope">
-          <el-button type="primary" @click="getBucketList">Refresh</el-button>
-        </template>
-      </el-table-column>
-      <el-table-column>
-        <template slot="header" slot-scope="scope">
-          <el-upload
-            class="upload-demo"
-            action=""
-            :show-file-list="false"
-            :http-request="handleFileChange"
-            accept=".XLSX, .XLSM, .XLS"
-          >
-            <el-button type="success">Upload</el-button>
-          </el-upload>
+      <el-table-column fixed="right" label="Operations" width="120">
+        <template slot-scope="scope">
+          <el-button link type="danger" @click="deleteFile(scope.row.Key)">
+            Remove
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -72,59 +74,72 @@ export default {
     this.getBucketList();
   },
   methods: {
-    handleFileChange(e) {
+    //handle upload file
+    async handleFileChange(e) {
       let file = e.file;
       console.log("file change", file);
       if (file) {
-        var params = {
+        let params = {
           Bucket: "compx576-bucket",
           Key: file.name,
           Body: file,
         };
-        if (this.checkBucket(params.Key)) {
-          this.$alert(
+        let isExist = await this.checkBucket(params.Key);
+        if (isExist) {
+          this.$confirm(
             "Same file exists, are you sure you want to overwrite it?",
             "Warning",
             {
               confirmButtonText: "Confirm",
-              callback: () => {
-                this.s3.upload(params, (err) => {
-                  if (err) {
-                    this.$message.warning(`${err.code},${err.data}`);
-                  } else {
-                    this.$message.success(`Upload success!`);
-                    this.getBucketList();
-                  }
-                });
-              },
+              cancelButtonText: "Cancel",
+              type: "warning",
             }
-          );
+          )
+            .then(() => {
+              this.s3.upload(params, (err) => {
+                if (err) {
+                  this.$message.warning(`${err.code},${err.data}`);
+                } else {
+                  this.$message.success(`Upload success!`);
+                  this.getBucketList();
+                }
+              });
+            })
+            .catch(() => {
+              this.$message.warning(`Cancelled`);
+            });
+        } else {
+          this.s3.upload(params, (err) => {
+            if (err) {
+              this.$message.warning(`${err.code},${err.data}`);
+            } else {
+              this.$message.success(`Upload success!`);
+              this.getBucketList();
+            }
+          });
         }
       }
     },
+    //pass file key
     async passFileName(key) {
       let res = await getFileContent({ key: key });
       this.dialogVisible = true;
       this.fileContent = res.data;
     },
-    async checkBucket(file) {
-      await this.s3
-        .headObject({
-          Bucket: "compx576-bucket",
-          Key: file,
-        })
-        .promise()
-        .then(
-          () => {
-            return true;
-          },
-          (err) => {
-            if (err.code === "NotFound") {
-              return false;
-            }
-            throw err;
-          }
-        );
+    //check if file exists in bucket by key
+    async checkBucket(fileKey) {
+      const params = {
+        Bucket: "compx576-bucket",
+        Key: fileKey,
+      };
+      try {
+        await this.s3.headObject(params).promise();
+        console.log("File Found in S3");
+        return true;
+      } catch (err) {
+        console.log("File not Found ERROR : " + err.code);
+        return false;
+      }
     },
     formatBytes(bytes) {
       const kb = 1024;
@@ -140,6 +155,16 @@ export default {
     async getBucketList() {
       let res = await getDataSource();
       this.tableData = res.data;
+    },
+    async deleteFile(fileKey) {
+      let params = { Bucket: "compx576-bucket", Key: fileKey };
+      try {
+        await this.s3.deleteObject(params).promise();
+        this.$message.success(`Delete success!`);
+        this.getBucketList();
+      } catch (err) {
+        this.$message.warning(err.code);
+      }
     },
   },
 };
